bd70b764543b90a282c390bbb1bbd594bb994f32
darkknight-005 <gauravmaurya61@yahoo.in>
2022-10-12 18:11:21 +0530

    .

--- components/menu.js
+++ components/menu.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Menu = ({Icon, Title, Name}) => {
   return (
     <li className='Menu text-white flex items-center gap-2 cursor-pointer px-1 py-2 justify-start space-y-2 tracking-widest group backdrop-sm bg-black/50 rounded-md' title={Name}>
-    <Icon className='text-2xl mr-1'/>
+    <Icon className='text-2xl'/>
     <h3 className='group-hover:block group-hover:text-red-100 hidden lg:block' id='title' >{Title}</h3>
   </li>
   )
